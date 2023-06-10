0414720d13f11e32a3142569701c551468e316ec
Rietta1 <riettabusiness@gmail.com>
2023-06-10 14:17:00 +0100

    Project 16

---
+++ 16ProjectIAC_Terraform/PBL/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = var.region
+}
+
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+  #   enable_classiclink             = "var.enable_classiclink"
+  #   enable_classiclink_dns_support = "var.enable_classiclink_dns_support"
+}
+
+# Get list of availability zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Create public subnets
+resource "aws_subnet" "public" {
+  count                   = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+}
+
---
+++ 16ProjectIAC_Terraform/PBL/terraform.tfvars
@@ -0,0 +1,13 @@
+region = "us-west-2"
+
+vpc_cidr = "172.16.0.0/16"
+
+enable_dns_support = "true"
+
+enable_dns_hostnames = "true"
+
+enable_classiclink = "false"
+
+enable_classiclink_dns_support = "false"
+
+preferred_number_of_public_subnets = 2
